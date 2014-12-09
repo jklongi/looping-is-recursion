337821157632dac17611d2be141675802e71b746
jklongi <joonas.longi@helsinki.fi>
2014-12-09 22:58:19 +0200

    102 dones?

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,10 +1,19 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (if (= exp 0)
+    1
+    (let [helper (fn [acc n k]
+                   (if (= 1 k)
+                     acc
+                     (recur (* acc n) n (dec k))))]
+      (helper base base exp))))
 
 (defn last-element [a-seq]
-  ":(")
+  (let [[x & xs] a-seq]
+      (if (empty? xs)
+        x
+        (last xs))))
 
 (defn seq= [seq1 seq2]
   ":(")
